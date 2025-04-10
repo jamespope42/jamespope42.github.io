60c96c548438291d40c3214917da6da2014d8cff
jamesbear44 <jjpope1@my.waketech.edu>
2025-04-10 03:31:24 -0400

    finished product
    done

---
+++ gradebook.js
@@ -0,0 +1,16 @@
+// gradebook.js
+
+function fetchGradeData() {
+    console.log("fetchGradeData stub called");
+    // Future: fetch data from PostgreSQL
+  }
+  
+  function populateGradebook() {
+    console.log("populateGradebook stub called");
+    // Future: dynamically insert rows into the table
+  }
+  
+  // Trigger the stubs to show they are working
+  fetchGradeData();
+  populateGradebook();
+  
